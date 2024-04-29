39482eefff842251746f5f5edc25ee45da790e09
Marcell Fulop <marcellfulop2000@gmail.com>
2024-04-29 07:16:31 +0000

    Code written for Service and Repo

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,57 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.repository.AccountRepository;
+import com.example.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    /**
+     * Adds account. Assumes it's formatted correctly.
+     * @param account
+     * @return the newly created account
+     */
+    public Account addAccount(Account account){
+        return accountRepository.save(account);
+    }
+
+    /**
+     * Login with a given username and password. Returns the account with its correct userID if correct and null if the password is wrong or the 
+     * account does not exist
+     * @param account
+     * @return
+     */
+    public Account login(Account account){
+        // have to manually thumb through all accounts to find a matching username
+        List<Account> allAccounts = accountRepository.findAll();
+        for (Account e : allAccounts){
+            if (e.getUsername().equals(account.getUsername())){
+                if (e.getPassword().equals(account.getPassword())){
+                    return e;
+                }
+                else{
+                    // wrong password
+                    return null;
+                }
+            }
+        }
+        // username did not exist in repository
+        return null;
+    }
+
+
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,71 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.repository.AccountRepository;
+import com.example.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MessageService {
+    AccountRepository accountRepository;
+    MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(AccountRepository accountRepository, MessageRepository messageRepository){
+        this.accountRepository = accountRepository;
+        this.messageRepository = messageRepository;
+    }
+
+    /**
+     * Adds a message. Assumes it's valid by following these parameters:
+     * 1. messageText is not empty nor greater than 255 characters
+     * 2. postedBy refers to a real existing account
+     * @return the newly minted message 
+     */
+    public Message postMessage(Message message){
+        return messageRepository.save(message);
+    }
+
+    /**
+     * @return all the messages 
+     */
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    /**
+     * Get a message by the unique ID. Returns null if none found.
+     * @param id
+     * @return
+     */
+    public Message getMessageByID(long id) {
+        Optional<Message> messageOptional = messageRepository.findById(id);
+        if (messageOptional.isPresent()){
+            return messageOptional.get();
+        }
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * Deletes message by its ID. Does not check if it actually exists.
+     */
+    public void deleteMessageByID(long id){
+        messageRepository.deleteById(id);
+    }
+
+    public Message updateMessage(Message message){
+        if (getMessageByID(message.getMessageId()) != null){
+
+        }
+        else{
+            return null;
+        }
+    }
 }
